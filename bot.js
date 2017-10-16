7538c27a7b7649d6639dfc8acb5effea342cac42
RedCoder4 <bd5@hotmail.com.au>
2017-10-16 21:15:49 +1100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+sconst Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'events') {
+    	message.reply('Well, this is sad! We appear to be in a blank spot. I am not able to find any events. Check back soon!');
   	}
 });
 
